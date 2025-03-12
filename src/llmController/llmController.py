e44ea3fd2f84addbf498479eb333ea1c3b39b478
Adrian Joost <adi.joost@gmail.com>
2025-03-12 17:06:11 +0100

    create basic structure

---
+++ src/llmController/llmController.py
@@ -0,0 +1,10 @@
+
+
+class LLM_Controller():
+
+    def __init__(self) -> None:
+        pass
+
+    def generate(self) -> str:
+        # call the LLM on ollama and return the answere.
+        pass
